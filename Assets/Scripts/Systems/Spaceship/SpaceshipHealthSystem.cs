af1af52447cd67776468e747820741f0f75ba3b8
Tadhg O'Rourke <orourketadhg@gmail.com>
2021-04-18 14:49:27 +0100

    spaceship destrustion

---
+++ Assets/Scripts/Systems/Spaceship/SpaceshipHealthSystem.cs
@@ -0,0 +1,36 @@
+﻿using Ie.TUDublin.GE2.Components.Spaceship;
+using Ie.TUDublin.GE2.Components.Tags;
+using Unity.Entities;
+
+namespace Ie.TUDublin.GE2.Systems.Spaceship {
+
+    public class SpaceshipHealthSystem : SystemBase {
+
+        private EndSimulationEntityCommandBufferSystem _entityCommandBufferSystem;
+        
+        protected override void OnCreate() {
+            _entityCommandBufferSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+            
+        }
+
+        protected override void OnUpdate() {
+
+            var ecb = _entityCommandBufferSystem.CreateCommandBuffer().AsParallelWriter();
+            
+            Entities
+                .WithName("SpaceshipCleanup")
+                .WithBurst()
+                .WithAll<SpaceshipTag>()
+                .WithAny<AlliedTag, EnemyTag>()
+                .ForEach((Entity entity, int entityInQueryIndex, in HealthData healthData) => {
+                    if (healthData.Value <= 0) {
+                        ecb.DestroyEntity(entityInQueryIndex, entity);
+                    }
+                }).ScheduleParallel();
+            
+            _entityCommandBufferSystem.AddJobHandleForProducer(Dependency);
+
+        }
+    }
+
+}
